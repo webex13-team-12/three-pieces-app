adebfc23465ee51b8a0f282e0c8486adc5614244
Ryota671 <rmdmatsuda@gmail.com>
2022-03-04 15:04:36 +0900

    画像がブラウザ上に表示されません

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -1,46 +1,83 @@
 <template>
   <h1>Vue クイズ</h1>
-  <div class="app">
-    <h2>Q. {{ "クイズタイトル" }}</h2>
-    <img
-      class="quiz-image"
-      src="https://via.placeholder.com/300x300"
-      alt="クイズタイトル"
-    />
+  <div id="app">
+    <h2>Q.{{ quiz.text }}</h2>
+    <h3>ヒント：東京上野の不忍池を洋画で描いた人物です。</h3>
+    <img id="quizImage" v-bind:src="quizImagePath" v-bind:alt="quiz.text" />
     <div class="container">
-      <button>
-        {{ "選択肢1" }}
-      </button>
-      <button>
-        {{ "選択肢2" }}
-      </button>
-      <button>
-        {{ "選択肢3" }}
+      <button
+        v-for="(choice, i) in quiz.choices"
+        v-bind:key="i"
+        v-on:click="choiced(choice)"
+      >
+        {{ choice.text }}
       </button>
     </div>
-    <div>{{ "答え" }}</div>
+    <div>{{ feedback }}</div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      feedback: "",
+      quiz: {
+        text: "日本で初めてコーヒーミルを作ったとされる人物はどれでしょう？",
+        image: "Coffee MIll.jpg",
+        choices: [
+          {
+            text: "平賀源内",
+            isCorrect: false,
+            feedback:
+              "残念!平賀源内は、エレキテルを修理した事で知られる蘭学者だよ!。",
+          },
+          {
+            text: "司馬江漢",
+            isCorrect: false,
+            feedback:
+              "正解!司馬江漢は、江戸時代後期に活躍した洋画家兼蘭学者。東海道五十三次のオリジナルを描いたとされる謎の多い人物だよ!!",
+          },
+          {
+            text: "大田南畝",
+            isCorrect: true,
+            feedback:
+              "惜しい!大田南畝は、日本で初めてコーヒーの味や風味を記録したとされる人物だよ!!",
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    choiced(choice) {
+      this.feedback = choice.feedback
+
+      if (choice.isCorrect) {
+        // 次の問題へ
+      }
+    },
+  },
+  computed: {
+    quizImagePath() {
+      return "./images/" + this.quiz.image
+    },
+  },
+}
 </script>
 
 <style>
-.app {
+#app {
   display: flex;
   width: 100%;
   flex-direction: column;
   align-items: center;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
-
-.quiz-image {
+#quiz-image {
   height: 300px;
   width: 300px;
   object-fit: contain;
 }
-
 .container {
   display: flex;
   height: 2em;
