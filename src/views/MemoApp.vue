1251731e175bddee4cccaaf89fbdee699aef447e
高橋直輝 <takahashinaoki@takahashinaokinoMacBook-Pro.local>
2022-03-05 16:03:13 +0900

    メモ機能コード全体的に修正しました。斜線着くようにした。

--- src/views/MemoApp.vue
+++ src/views/MemoApp.vue
@@ -2,12 +2,26 @@
   <h1>Vue メモ</h1>
   <div class="memo-list">
     <ul class="memo-list__container">
-      <Memo
+      <!-- <Memo
         v-for="memo in memos"
         v-bind:key="memo.id"
         v-bind:memo="memo"
         @my-click="reflectNum"
-      />
+      /> -->
+      <li
+        class="memo"
+        v-for="memo in memos"
+        v-bind:key="memo.id"
+        v-bind:memo="memo"
+      >
+        <div class="memo__checkbox">
+          <input type="checkbox" v-on:click="cheak" v-model="memo.situation" />
+        </div>
+        <div class="memo__text" :class="{ 'memo__text--done': memo.situation }">
+          {{ memo.text }}
+        </div>
+        <button class="memo__delete" @click="deleteTask(memo.id)">削除</button>
+      </li>
     </ul>
     <div class="add-memo-field">
       <input class="add-memo-field__input" type="text" v-model="inputMemo" />
@@ -17,9 +31,7 @@
 </template>
 
 <script>
-import Memo from "@/components/Memo.vue"
 export default {
-  components: { Memo },
   data() {
     return {
       memos: [],
@@ -28,11 +40,15 @@ export default {
   },
   methods: {
     addMemo() {
-      let memo = { id: this.memos.length, text: this.inputMemo }
+      let memo = {
+        id: this.memos.length,
+        text: this.inputMemo,
+        situation: false,
+      }
       this.inputMemo = ""
       this.memos.push(memo)
     },
-    reflectNum(id) {
+    deleteTask(id) {
       this.memos.splice(id, 1)
     },
   },
