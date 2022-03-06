2d707471b6ba80d2003fc32bf1454d6600e043ca
Ryota671 <rmdmatsuda@gmail.com>
2022-03-06 16:43:56 +0900

    画像が表示されました

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div>
       This Application is made by
-      <span class="names">○○○, ○○○ and ○○○</span>.
+      <span class="names">, なおき and みゆ and まっつー </span>.
     </div>
     <small
       >The Application is created as a part of the GeekSalon WebExpert
